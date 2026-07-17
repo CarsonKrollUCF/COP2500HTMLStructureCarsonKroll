ea17abb0a20973140671c23dd62c66b9a4d809c5
CarsonKrollUCF <ca064257@ucf.edu>
2026-07-17 11:08:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector(".interaction");
 const main = document.querySelector(".main");
 
+// addEventListener tells the website to wait for a click interaction on a certain aspect, in this case the "interaction" div, and does a certain action based on that interaction,
+// in this case, it swaps the site theme.
 button.addEventListener("click", () => {
     main.classList.toggle("evil");
 });
